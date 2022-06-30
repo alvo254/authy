13791f1b0b080d055e20b05de4dab4ec767e085b
alvo254 <alvinkhamron@gmail.con>
2022-06-30 14:44:41 +0300

    jwt addition

--- src/main.go
+++ src/main.go
@@ -4,12 +4,17 @@ import (
 	"./database"
 	"./routes"
 	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/middleware/cors"
 )
 
 func main() {
+	//fmt.Println("hello")
 	database.Connect()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 	routes.Setup(app)
-	app.Listen(":8000")
+	app.Listen(":9000")
 
 }
